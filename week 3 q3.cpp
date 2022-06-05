5a720270563614448e9edbfa22bfce458a1983f1
udittomar28 <100894555+udittomar28@users.noreply.github.com>
2022-06-05 21:41:52 +0530

    Create week 3 q3.cpp

---
+++ week 3 q3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        vector <int> nums;
+        int n;
+        int a1;
+        for (int i=0;i<n;i++)
+        {
+            cin>>a1;
+            nums.push_back(a1);
+        }
+    }
+}
